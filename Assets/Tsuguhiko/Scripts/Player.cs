d322092dbfc6d5478358802bd47d1bc2fe3b2320
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2022-06-26 13:49:01 +0900

    メインゲームシーンの背景スクロール動作を追加。

--- Assets/Tsuguhiko/Scripts/Player.cs
+++ Assets/Tsuguhiko/Scripts/Player.cs
@@ -6,49 +6,49 @@
 public class Player : MonoBehaviour
 {
 
-	[SerializeField] float speed = 2; // スピード：Inspectorで指定
+	[SerializeField] float _speed = 2; // スピード：Inspectorで指定
 
-	float vx = 0;
-	float vy = 0;
+	float _vx = 0;
+	float _vy = 0;
 
-	bool leftFlag = false;
+	bool _leftFlag = false;
 
-	Rigidbody2D rbody;
+	Rigidbody2D _rbody;
 
 	void Start () 
 	{
-		rbody = GetComponent<Rigidbody2D>();
+		_rbody = GetComponent<Rigidbody2D>();
 		
-		rbody.constraints = RigidbodyConstraints2D.FreezeRotation; // 衝突時に回転させない
+		_rbody.constraints = RigidbodyConstraints2D.FreezeRotation; // 衝突時に回転させない
 	}
 
 	void Update () 
 	{ 
-		vx = 0;
-		vy = 0;
+		_vx = 0;
+		_vy = 0;
 		if(Input.GetKey("right")) // 右キーが押されたら
 		{ 
-			vx = speed; // 右に進む移動量を入れる
-			leftFlag = false;
+			_vx = _speed; // 右に進む移動量を入れる
+			_leftFlag = false;
 		}
 		if(Input.GetKey("left")) // 左キーが押されたら
 		{ 
-			vx = -speed; // 左に進む移動量を入れる
-			leftFlag = true;
+			_vx = -_speed; // 左に進む移動量を入れる
+			_leftFlag = true;
 		}
 		if(Input.GetKey("up")) // 上キーが押されたら
 		{ 
-			vy = speed; // 上に進む移動量を入れる
+			_vy = _speed; // 上に進む移動量を入れる
 		}
 		if(Input.GetKey("down")) // 下キーが押されたら
 		{ 
-			vy = -speed; // 下に進む移動量を入れる
+			_vy = -_speed; // 下に進む移動量を入れる
 		}
 	}
 	void FixedUpdate() 
 	{
-		rbody.velocity = new Vector2(vx, vy); // 移動処理
+		_rbody.velocity = new Vector2(_vx, _vy); // 移動処理
 											  
-		this.GetComponent<SpriteRenderer>().flipX = leftFlag; // 左右の向きを変える
+		this.GetComponent<SpriteRenderer>().flipX = _leftFlag; // 左右の向きを変える
 	}
 }
